ad9898629ec1d3c4a7da056b683dd2b3faf8fcba
mohdshahzil <theshahzil@gmail.com>
2025-04-03 16:36:55 +0530

    clerk added

--- app/student-login/page.tsx
+++ app/student-login/page.tsx
@@ -2,6 +2,13 @@ import React from "react";
 import AppbarNoLogin from "@/components/Appbar/AppbarNoLogin";
 import Footer from "@/components/Footer";
 import Login from "@/components/LoginForm/Loginform";
+import {
+  SignInButton,
+  SignUpButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from '@clerk/nextjs'
 const page = () => {
   return (
     <div>
--- components/LoginForm/Loginform.tsx
+++ components/LoginForm/Loginform.tsx
@@ -1,33 +1,68 @@
+// "use client";
+// import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
+// import Link from "next/link";
+// import Image from "next/image";
+
+// export default function Login() {
+//   return (
+//     <div className="relative flex flex-col items-center justify-center min-h-screen overflow-hidden">
+//       <div className="w-full p-6 bg-white rounded-md shadow-lg lg:max-w-xl mb-40">
+//         <div className="w-full h-1 bg-primary mb-10"></div>
+//         <h1 className="font-bold text-center text-black text-3xl">
+//           VHTOP Login
+//         </h1>
+
+//         <div className="mt-2">
+//           <div className="flex items-center justify-center dark:bg-gray-800">
+//             <SignedOut>
+//               <SignInButton mode="redirect">
+//                 <button className="px-4 py-2 border flex gap-2 border-slate-200 dark:border-slate-700 rounded-lg text-slate-700 dark:text-slate-200 hover:border-slate-400 dark:hover:border-slate-500 hover:text-slate-900 dark:hover:text-slate-300 hover:shadow transition duration-150">
+//                   <Image
+//                     src="https://www.svgrepo.com/show/475656/google-color.svg"
+//                     loading="lazy"
+//                     alt="google logo"
+//                     width={24}
+//                     height={24}
+//                   />
+//                   <span>Login with Google</span>
+//                 </button>
+//               </SignInButton>
+//             </SignedOut>
+
+//             <SignedIn>
+//               <UserButton />
+//             </SignedIn>
+//           </div>
+//         </div>
+
+//         <p className="mt-2 text-sm text-right text-white">
+//           <Link href="/" className="text-xs text-green-600 hover:underline">
+//             Return to Home Page
+//           </Link>
+//         </p>
+//       </div>
+//     </div>
+//   );
+// }
+
 "use client";
-import createStudentAndStoreEmail from "@/app/Hooks/SignIn";
+import { SignInButton, SignedIn, SignedOut, UserButton, useAuth, useUser } from "@clerk/nextjs";
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
 import Link from "next/link";
-import { useState, ChangeEvent } from "react";
-import useUserStore from "@/app/store/store";
 import Image from "next/image";
+
 export default function Login() {
-  const [email, setEmail] = useState<string>("");
-  const [password, setPassword] = useState<string>("");
-  const setUser = useUserStore((state) => state.setUser);
-  const use = useUserStore((state) => state.user);
-  const handleEmailChange = (event: ChangeEvent<HTMLInputElement>) => {
-    setEmail(event.target.value);
-  };
+  const { isSignedIn } = useAuth();
+  const { user } = useUser();
+  const router = useRouter();
 
-  const handlePasswordChange = (event: ChangeEvent<HTMLInputElement>) => {
-    setPassword(event.target.value);
-  };
-
-  const usercreate = async (
-    event: React.MouseEvent<HTMLButtonElement, MouseEvent>
-  ) => {
-    event.preventDefault();
-    const user = await createStudentAndStoreEmail();
-    if (user) {
-      setUser(user);
-      window.location.href = "/content";
+  // Redirect to /content if the user is already signed in
+  useEffect(() => {
+    if (isSignedIn && user) {
+      router.push("/content");
     }
-    console.log(use);
-  };
+  }, [isSignedIn, user, router]);
 
   return (
     <div className="relative flex flex-col items-center justify-center min-h-screen overflow-hidden">
@@ -36,72 +71,30 @@ export default function Login() {
         <h1 className="font-bold text-center text-black text-3xl">
           VHTOP Login
         </h1>
-        <form className="mt-6">
-          {/* <div className="mb-4">
-            <label
-              htmlFor="email"
-              className="block text-sm font-semibold text-primary"
-            >
-              Email
-            </label>
-            <input
-              type="email"
-              id="email"
-              value={email}
-              onChange={handleEmailChange}
-              className="block w-full px-4 py-2 mt-2 text-black bg-white border rounded-md focus:border-gray-400 focus:ring-gray-300 focus:outline-none focus:ring focus:ring-opacity-40"
-            />
-          </div> */}
-          {/* <div className="mb-2">
-            <label
-              htmlFor="password"
-              className="block text-sm font-semibold text-primary"
-            >
-              Password
-            </label>
-            <input
-              type="password"
-              id="password"
-              value={password}
-              onChange={handlePasswordChange}
-              className="block w-full px-4 py-2 mt-2 text-black bg-white border rounded-md focus:border-gray-400 focus:ring-gray-300 focus:outline-none focus:ring focus:ring-opacity-40"
-            />
-          </div> */}
-          <div className="mt-2">
-            {/* <button
-              type="submit"
-              className="w-full px-4 py-2 tracking-wide text-primary transition-colors duration-200 transform bg-primary text-white rounded-md hover:bg-primary hover:text-white focus:outline-none focus:bg-primary focus:text-white"
-              onClick={(event) => usercreate(event)}
-            >
-              Login
-            </button> */}
-            <div className="flex items-center justify-center dark:bg-gray-800">
-              <button
-                type="submit"
-                onClick={(event) => usercreate(event)}
-                className="px-4 py-2 border flex gap-2 border-slate-200 dark:border-slate-700 rounded-lg text-slate-700 dark:text-slate-200 hover:border-slate-400 dark:hover:border-slate-500 hover:text-slate-900 dark:hover:text-slate-300 hover:shadow transition duration-150"
-              >
-                <Image
-                  className=""
-                  src="https://www.svgrepo.com/show/475656/google-color.svg"
-                  loading="lazy"
-                  alt="google logo"
-                  width={24}
-                  height={24}
-                />
-                <span>Login with Google</span>
-              </button>
-            </div>
+
+        <div className="mt-2">
+          <div className="flex items-center justify-center dark:bg-gray-800">
+            <SignedOut>
+              <SignInButton>
+                <button className="px-4 py-2 border flex gap-2 border-slate-200 dark:border-slate-700 rounded-lg text-slate-700 dark:text-slate-200 hover:border-slate-400 dark:hover:border-slate-500 hover:text-slate-900 dark:hover:text-slate-300 hover:shadow transition duration-150">
+                  <Image
+                    src="https://www.svgrepo.com/show/475656/google-color.svg"
+                    loading="lazy"
+                    alt="google logo"
+                    width={24}
+                    height={24}
+                  />
+                  <span>Login with Google</span>
+                </button>
+              </SignInButton>
+            </SignedOut>
+
+            <SignedIn>
+              <UserButton />
+            </SignedIn>
           </div>
-        </form>
-        <p className="mt-2 text-sm text-right text-white">
-          <Link
-            href="/forget"
-            className="text-xs text-blue-600 hover:underline"
-          >
-            Forget Password?
-          </Link>
-        </p>
+        </div>
+
         <p className="mt-2 text-sm text-right text-white">
           <Link href="/" className="text-xs text-green-600 hover:underline">
             Return to Home Page
@@ -111,3 +104,4 @@ export default function Login() {
     </div>
   );
 }
+
